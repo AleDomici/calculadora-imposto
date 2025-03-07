dec5c6bf2dd93c42d61e384a9127b7e3da0b0f5f
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-03-07 17:33:16 -0300

    Adiciona o CalculadoraImpostoApplicationTests"

---
+++ calculadoraImposto/src/test/java/com/catalisa/calculadoraImposto/CalculadoraImpostoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa.calculadoraImposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraImpostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
