15dd1f975506b9858e3995c7e630c9fb6a18bf7e
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-03-11 09:20:58 -0300

    feat(dto): adicionar classe ImpostoResponse para encapsulamento de respostas de dados
    Este commit introduz a classe ImpostoResponse, que inclui:
    - Campos: id (Long), nome (String), descricao (String), aliquota (Double).
    - Construtor parametrizado para inicialização de todos os campos.
    - Métodos getters e setters para acesso e manipulação dos dados encapsulados.
    - Responsável por representar a resposta de dados no contexto de impostos, garantindo a separação entre a entidade de domínio e a camada de transporte de dados

---
+++ calculadoraImposto/src/main/java/com/catalisa/calculadoraImposto/dto/ImpostoResponse.java
@@ -0,0 +1,49 @@
+package com.catalisa.calculadoraImposto.dto;
+
+public class ImpostoResponse {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double aliquota;
+
+    // Construtor
+    public ImpostoResponse(Long id, String nome, String descricao, Double aliquota) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.aliquota = aliquota;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getAliquota() {
+        return aliquota;
+    }
+
+    public void setAliquota(Double aliquota) {
+        this.aliquota = aliquota;
+    }
+}
