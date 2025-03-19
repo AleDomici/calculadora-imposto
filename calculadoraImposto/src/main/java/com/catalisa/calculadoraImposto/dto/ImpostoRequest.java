71d5bbfa2e34cbc371ba47fb4ba89eaec7bbd71c
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-03-19 15:01:39 -0300

    feat(dto): adiciona validações nos campos do ImpostoRequest
    - Adiciona validação para garantir que o campo 'nome' não seja nulo ou vazio.
    - Adiciona validação para garantir que o campo 'descricao' não seja nulo ou vazio.
    - Adiciona validação para garantir que o campo 'aliquota' seja positivo e não nulo.
    - Melhora a consistência dos dados recebidos no DTO `ImpostoRequest`.

--- calculadoraImposto/src/main/java/com/catalisa/calculadoraImposto/dto/ImpostoRequest.java
+++ calculadoraImposto/src/main/java/com/catalisa/calculadoraImposto/dto/ImpostoRequest.java
@@ -1,8 +1,19 @@
 package com.catalisa.calculadoraImposto.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public class ImpostoRequest {
+
+    @NotBlank(message = "O campo 'nome' é obrigatório e não pode estar vazio.")
     private String nome;
+
+    @NotBlank(message = "O campo 'descricao' é obrigatório e não pode estar vazio.")
     private String descricao;
+
+    @NotNull(message = "O campo 'aliquota' é obrigatório.")
+    @Positive(message = "O campo 'aliquota' deve ser maior que zero.")
     private Double aliquota;
 
     public ImpostoRequest() {
